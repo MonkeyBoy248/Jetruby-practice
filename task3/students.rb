4b79a36b6d9dc8bd3b23eea60dae25adeb596de0
MonkeyBoy248 <kirik201100@mail.ru>
2022-02-11 12:58:54 +0300

    Refactored the algorithm
    Added an additional array to check whether the data has already been written to the result file

--- task3/students.rb
+++ task3/students.rb
@@ -4,16 +4,20 @@ RESULT_PATH = './task3/result.txt'
 def check_age
   file = File.open(STUDENTS_PATH)
   students = file.readlines.map { |line| line }
+  checked_students = []
   file.close
   age = ''
 
-  while age != "-1" && !students.empty?
+  while age != "-1" && checked_students.length != students.length
     puts 'Введите искомый возраст'
     age = STDIN.gets.chomp
     target_students = students.select { |student| student.slice(/\d+/) == age}
 
     target_students.each do |student|
-      File.write(RESULT_PATH, students.delete(student), mode: 'a')
+      if(!checked_students.include?(student))
+        File.write(RESULT_PATH, student, mode: 'a')
+        checked_students.push(student)
+      end
     end
   end
   File.read(RESULT_PATH)
